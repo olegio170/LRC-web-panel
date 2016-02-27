3fc2cd71f36942c87d3d91436743d4a75d446a5e
olegio170 <o1999@ukr.net>
2016-02-27 14:13:37 +0200

    Fix user control

--- js/userControl.js
+++ js/userControl.js
@@ -3,33 +3,36 @@ var id = '8f51dfd14221bc7d6adaefdf0533bf9e2af2d21e1395c6085fdf76943734c271';
 var clientObj = {
     isAnswer:false
 }
-
-main();
+$(document).ready(function(){
+    main();
+});
+//main();
 function main (){
     //comand_line();
     if (!window.WebSocket) {
         alert ('WebSocket is  unsupported in your browser!');
     }
-    connect_to_server();
+    connectToServer();
     $('.getButton').click(function(){
         if(clientObj['isAnswer']) {
             var request = $(this).attr('title');
-            send_request(request, 'request');
-            check_answer_from_server();
+            sendRequest(request, 'request');
+            checkAnswerFromServer();
         }
     });
 }
-function check_answer_from_server() {
+function checkAnswerFromServer() {
     setTimeout(function () {
         if (!clientObj['isAnswer'])
         {
-            write_console('[ERROR] server is offline now !');
+            writeConsole('[ERROR] server is offline now !');
             ws.close();
-            connect_to_server();
+            connectToServer();
         }
     }, 5000);
 }
-function send_request (request,type) {
+
+function sendRequest (request, type) {
     var targetId = $('#divId').text();
     var obj = {
         request:request,
@@ -49,37 +52,47 @@ function send_request (request,type) {
         console.log('isAnswer ' + clientObj['isAnswer']);
     },250);
 }
-function connect_to_server () {
+
+function connectToServer () {
     console.log('connect');
     ws = new WebSocket("ws://5.58.91.27:25565");
+    clientObj['isAnswer'] = false;
 
-    define_ws_handler();
+    defineWsHandler();
+    checkAnswerFromServer();
+}
+
+function sendAuthData () {
     var objOutput = {
         id:id,
         type:'auth'
     }
 
     var output = JSON.stringify(objOutput);
-    setTimeout(function() {
-        clientObj['isAnswer'] = false;
-        console.log('isAnswer ' + clientObj['isAnswer']);
-        ws.send(output);
-        console.log("output : "+ output);
-    }, 250);
-    check_answer_from_server();
+    clientObj['isAnswer'] = false;
+    console.log('isAnswer ' + clientObj['isAnswer']);
+    ws.send(output);
+    console.log("output : "+ output);
 }
-//$(document).ready(function(){main ();});
-function define_ws_handler () {
+
+function defineWsHandler () {
     ws.onmessage = function (message) {
         clientObj['isAnswer'] = true;
         console.log('isAnswer ' + clientObj['isAnswer']);
-        write_console(message['data']);
+        writeConsole(message['data']);
     };
     ws.onclose = function () {
-
-    }
+        if(clientObj['isAnswer'])
+        {
+            connectToServer();
+        }
+    };
+    ws.onopen = function () {
+        sendAuthData();
+    };
 }
-function write_console (msg) {
+
+function writeConsole (msg) {
     var now = new Date();
     //In order to show time with "0"
     var time=('0'  + now.getHours()).slice(-2)+':'+('0'  + now.getMinutes()).slice(-2)+':'+('0' + now.getSeconds()).slice(-2);
@@ -87,6 +100,7 @@ function write_console (msg) {
     $('#answerBox').append('<div>' + message + '</div>');
     document.getElementById('answerBox').scrollTop = 9999;
 }
+
 function comand_line (){
     setInterval(function() {
         $('#consoleChar').hide();
